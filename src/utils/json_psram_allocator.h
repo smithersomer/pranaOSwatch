9dfd8da2cf15e263af091c76a3e633b226696650
krishpranav <krisna.pranav@gmail.com>
2021-08-29 18:34:17 +0530

    src: utils json ps ram allocator
    updated json ps ram allocator header

--- src/utils/json_psram_allocator.h
+++ src/utils/json_psram_allocator.h
@@ -1,11 +1,16 @@
 #ifndef SPI_RAM_JSON_DOC_H_
 #define SPI_RAM_JSON_DOC_H_
 
+// includes
+#include "config.h"
+#include "ArduinoJson.h"
+#include "alloc.h"
+
 struct SpiRamAllocator {
     void* allocate(size_t size) {
-        void *ram = MALLOC( size );
-        if ( ram ) {
-            return( ram );
+        void *ram = MALLOC(size);
+        if (ram) {
+            return(ram);
         }
         else {
             log_e("allocate %d bytes (%p) json psram failed", size, ram );
@@ -18,8 +23,20 @@ struct SpiRamAllocator {
     void deallocate(void* pointer) {
         free(pointer);
     }
+    void *reallocate(void *pointer, size_t size) {
+        void *ram = REALLOC(pointer, size);
+        if (ram) {
+            return(ram);
+        }
+        else {
+            log_e("rallocate %d bytes (%p) json psram failed", size, ram );
+            if ( size == 0 ) {
+                log_e("rallocate zero bytes? really? abort");
+            }
+            return(ram);
+        }
+    }
 };
-
 using SpiRamJsonDocument = BasicJsonDocument<SpiRamAllocator>;
 
 #endif
