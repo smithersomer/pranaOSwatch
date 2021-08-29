71d98b8996666e79fd9c4cd024600af48c89e4c6
krishpranav <krisna.pranav@gmail.com>
2021-08-29 18:00:07 +0530

    src: utils basejsonconfig[private, protected]

---
+++ src/utils/basejsonconfig.h
@@ -0,0 +1,35 @@
+#ifndef BASEJSONCONFIG_H_
+#define BASEJSONCONFIG_H_
+
+#define MAX_CONFIG_FILE_NAME_LENGTH 32
+
+#include "ArduinoJson.h"
+
+
+class BaseJsonConfig {
+public:
+    BaseJsonConfig(const char* configFileName);
+
+    bool load();
+
+    bool load(uint32_t size);
+
+    bool save();
+
+    bool save(uint32_t size);
+
+    void debugPrint();
+
+protected:
+    virtual bool onSave(JsonDocument& document) = 0;
+    virtual bool onLoad(JsonDocument& document) = 0;
+
+    virtual bool onDefault(void) = 0;
+    virtual size_t getJSonBufferSize() { return 0; }
+
+protected:
+    bool prettyJson = true;
+
+}; 
+
+#endif 
